b119d84300bf9d3634f3919eec325e531a6eec33
waheeb4 <adhamwaheeb4444@gmail.com>
2026-04-28 17:09:39 +0300

    First bash script version

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+set -e
+
+docker stop db-container || true
+docker rm db-container || true
+
+docker network rm dxc-network || true
+
+docker network create dxc-network
+
+docker build -f database.Dockerfile -t database:1.0 . &
+databasepid=$!
+
+wait $databasepid
+docker run --name db-container --network dxc-network --mount type=volume,src=db-data,dst=/var/lib/mysql database:1.0
+
