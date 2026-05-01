fd818b7d584e98392d7c1f48fa6d6711f05880f4
waheeb4 <adhamwaheeb4444@gmail.com>
2026-05-01 15:53:32 +0300

    sleep loop to prevent db race conditions

--- build.sh
+++ build.sh
@@ -21,4 +21,9 @@ backendpid=$!
 wait $databasepid $backendpid
 
 docker run --name db-container --network dxc-network --mount type=volume,src=db-data,dst=/var/lib/mysql -d database:1.0
+
+until docker run --rm --network dxc-network mysql:latest mysqladmin ping -h db-container -uroot -pmy-pwd --silent 2>/dev/null; do
+    sleep 2
+done
+
 docker run --name backend-container --network dxc-network -p 8080:8080 -d backend:1.0
