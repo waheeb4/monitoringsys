4af5f2245c11d6ae9498941ec81f7445acb43844
waheeb4 <adhamwaheeb4444@gmail.com>
2026-05-01 15:50:29 +0300

    removed test backend and added dxc-backend submodule

--- backend/src/main/java/com/dxc/monitoring/repository/DeviceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dxc.monitoring.repository;
-
-import com.dxc.monitoring.model.Device;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceRepository extends JpaRepository<Device, Integer> {
-}
